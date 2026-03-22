02a0369d90f4f7a353f5c988a6ff3e9d390c2d5f
Chaeng-Ploypaphat-S <ploypaphat.saltz@gmail.com>
2026-03-21 18:41:42 -0700

    [feat]: implement postgres query to a movie and a movie edit

--- go-movies-back-end/internal/repository/repository.go
+++ go-movies-back-end/internal/repository/repository.go
@@ -10,4 +10,6 @@ type DatabaseRepo interface {
 	Connection() *sql.DB
 	GetUserByEmail(email string) (*models.User, error)
 	GetUserByID(userID int) (*models.User, error)
+	OneMovie(id int) (*models.Movie, error)
+	OneMovieForEdit(id int) (*models.Movie, []*models.Genre, error)
 }
